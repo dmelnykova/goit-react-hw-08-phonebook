72354f3a57babef50c0ff637af69db96e6aa8120
dariameln1 <dariamelnykova1@gmail.com>
2024-01-31 12:16:54 +0100

    Update ContactsPage.jsx

--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -1,9 +1,18 @@
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { fetchContacts } from '../../redux/contacts/operations';
 import { ContactForm } from "../../components/ContactForm/ContactForm";
 import { ContactsList } from "../../components/ContactsList/ContactsList";
 import { FilterField } from "../../components/FilterField/Filter";
 import { PhoneFormWrapper, FilterListWrapper } from "./ContactsPage.styled";
 
 export default function ContactPage() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]); 
+
   return (
     <PhoneFormWrapper>
       <ContactForm />
